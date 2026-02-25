68c942456a3dc12075623c736ff95dc91ddf8489
Jimena Estrada <mjestradad@ufm.edu>
2026-02-25 06:51:24 -0600

    Angular terminado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,34 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header';
+import { About } from './components/about/about';
 import { SidebarComponent } from './components/sidebar/sidebar';
+import { Experience } from './experience/experience';
+import { Education } from './education/education';
+import { References } from './references/references';
+import { Certifications } from './components/certifications/certifications';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     HeaderComponent,
-    SidebarComponent
+    About,
+    SidebarComponent,
+    Experience,
+    Education,
+    References,
+    Certifications
   ],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
-export class App {}
+export class App {
+
+  isDark = false;
+
+  toggleDarkMode() {
+    this.isDark = !this.isDark;
+  }
+
+}
--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -7,3 +7,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.css']
 })
 export class SidebarComponent {}
+
